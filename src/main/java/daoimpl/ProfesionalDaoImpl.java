f1635df37f99abed76b32f4366c5291ad4413391
Livio <livio.gutierrez.v@gmail.com>
2023-06-25 13:24:36 -0400

    actualizando profesional crud

--- src/main/java/daoimpl/ProfesionalDaoImpl.java
+++ src/main/java/daoimpl/ProfesionalDaoImpl.java
@@ -1,28 +1,28 @@
 package daoimpl;
 
 import dao.IProfesional;
+import models.Profesional;
 
 import java.util.List;
 
 public class ProfesionalDaoImpl implements IProfesional {
-
     @Override
-    public boolean crear(Profesional profesional) {
+    public boolean create(Profesional profesional) {
         return false;
     }
 
     @Override
-    public boolean actualizar(int id) {
+    public boolean update(int id) {
         return false;
     }
 
     @Override
-    public boolean eliminar(int id) {
+    public boolean delete(int id) {
         return false;
     }
 
     @Override
-    public List<Profesional> lista() {
+    public List<Profesional> list() {
         return null;
     }
 }
