ff3089e04e1de239189e5c815fa5d7cc5558674f
Jucame <jcmerino@uc.cl>
2023-06-28 11:58:12 -0400

    cambios menores

--- src/main/java/models/Administrativo.java
+++ src/main/java/models/Administrativo.java
@@ -2,6 +2,8 @@ package models;
 
 import lombok.*;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -11,4 +13,17 @@ public class Administrativo extends Usuario{
     private String area;
     private String experienciaPrevia;
 
+    public Administrativo(String nombre,
+                          String apellido1,
+                          String apellido2,
+                          Date fechaNacimiento,
+                          int run,
+                          String password,
+                          int tipo_usuario,
+                          String area,
+                          String experienciaPrevia) {
+        super(nombre, apellido1, apellido2, fechaNacimiento, run, password, tipo_usuario);
+        this.area = area;
+        this.experienciaPrevia = experienciaPrevia;
+    }
 }
